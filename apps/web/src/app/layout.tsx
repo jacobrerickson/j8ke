c3aba153ce77c8637005e202647db63e92a192af
Jacob Erickson <143523341+jacobrerickson@users.noreply.github.com>
2025-10-16 15:04:49 -0600

    wasn't wrapping layout with providers lol

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="tw-h-full">
       <body className={`${inter.className} tw-h-full`}>
+        <Providers>
           <Suspense fallback={<div className="tw-flex tw-justify-center tw-items-center tw-min-h-screen">
             <div className="tw-flex tw-flex-col tw-items-center tw-gap-4">
               <div className="tw-animate-spin tw-h-8 tw-w-8 tw-border-4 tw-border-blue-600 tw-border-t-transparent tw-rounded-full"></div>
@@ -27,6 +28,7 @@ export default function RootLayout({
           </div>}>
             {children}
           </Suspense>
+        </Providers>
       </body>
     </html>
   );
