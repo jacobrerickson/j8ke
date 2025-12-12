4ffcc37aaf7ed322a7651cda636ddfac6f847031
Jacob Erickson <143523341+jacobrerickson@users.noreply.github.com>
2025-12-12 12:54:42 -0700

    skipping db connection

--- packages/server/src/db/index.ts
+++ packages/server/src/db/index.ts
@@ -1,7 +1,7 @@
 import { connect } from 'mongoose'
-
-connect(process.env.DB_URL!).then(() => {
-    console.log('Connected to MongoDB')
-}).catch((err) => {
-    console.log('Failed to connect to MongoDB: ', err.message)
-})
+console.log("skipping db connection")
+// connect(process.env.DB_URL!).then(() => {
+//     console.log('Connected to MongoDB')
+// }).catch((err) => {
+//     console.log('Failed to connect to MongoDB: ', err.message)
+// })
